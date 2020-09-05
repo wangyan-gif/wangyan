e1bda7c4265915349b3e41a3bba753cf79261a3e
wangyan <3045559812@qq.com>
2020-09-05 12:07:55 +0800

    初始版本-修改1

--- b2c-order/src/main/java/com/mr/order/service/VipOrderService.java
+++ b2c-order/src/main/java/com/mr/order/service/VipOrderService.java
@@ -1,8 +1,11 @@
 package com.mr.order.service;
 
+import com.mr.bo.UserInfo;
 import com.mr.order.mapper.VipOrderMapper;
+import com.mr.order.pojo.Vip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class VipOrderService {
@@ -10,5 +13,10 @@ public class VipOrderService {
     @Autowired
     private VipOrderMapper vipOrderMapper;
 
+    @Transactional
+    public Long vipRecharge(Vip vip, UserInfo userInfo) {
 
+
+        return vip.getUserId();
+    }
 }
--- b2c-order/src/main/java/com/mr/order/web/VipOrderController.java
+++ b2c-order/src/main/java/com/mr/order/web/VipOrderController.java
@@ -1,9 +1,11 @@
 package com.mr.order.web;
 
+import com.mr.bo.UserInfo;
 import com.mr.common.utils.CookieUtils;
 import com.mr.config.JwtConfig;
 import com.mr.order.pojo.Vip;
 import com.mr.order.service.VipOrderService;
+import com.mr.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +26,18 @@ public class VipOrderController {
     // 月份 数字/字符串
     // username
 
-    @PostMapping("vipRecharge")
+    @PostMapping("userUp")
     public ResponseEntity<Long> vipRecharge(@RequestBody Vip vip, HttpServletRequest request){
         String token = CookieUtils.getCookieValue(request, jwtConfig.getCookieName());
 
-
+        try {
+            //获取会员登录数据
+            UserInfo userInfo = JwtUtils.getInfoFromToken(token, jwtConfig.getPublicKey());
+            Long id = vipOrderService.vipRecharge(vip,userInfo);
+            return new  ResponseEntity<>(id,HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
